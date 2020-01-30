ea52d5e063c2de76007727cec471edd6924d8129
coltonfalkner <coltonfalkner@gmail.com>
2020-01-29 18:04:36 -0600

    connection tweaks

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
-var connection;
+
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    port: 3306,
+    // port: 3306,
     host: 'localhost',
     user: 'root',
     password: 'Ilovealthea13!',
